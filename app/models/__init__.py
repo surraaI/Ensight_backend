8a9659c0d48e6bfe9a1230c3aae06a72f25985e0
Sura Itana <suraitana@gmail.com>
2025-10-21 13:04:25 +0300

    fix issues

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
+
 from app.models.user import User, Role
 from app.models.profile import Profile
 from .article import Article  
-=======
+
 # Import order matters - put simple models first
 from .user import User
 from .profile import Profile
->>>>>>> 559d8bebe8bae22b33178d06e50b96e739d1860f
+
 from .subscription_plan import SubscriptionPlan
 from .article import Article
 
